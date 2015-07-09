a02f2a85082f768efac576a91b4739a056730d73
foxtrot-developer <foxtrot-developer@users.noreply.github.com>
2015-07-09 18:29:26 +0200

    Update BrandSpecificController.java

--- src/java/com/development/simplestockmanager/business/object/controller/specific/BrandSpecificController.java
+++ src/java/com/development/simplestockmanager/business/object/controller/specific/BrandSpecificController.java
@@ -4,26 +4,30 @@ import com.development.simplestockmanager.business.object.helper.BrandHelper;
 import com.development.simplestockmanager.business.object.nullpackage.BrandNull;
 import com.development.simplestockmanager.business.persistence.Brand;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Query;
 
 /**
+ * Specific controller class for Brand object
  *
  * @author foxtrot
  */
 public class BrandSpecificController {
-    
+
     private final BrandHelper helper;
 
     public BrandSpecificController() {
         helper = new BrandHelper();
     }
 
-    public List<Brand> findAll() {
+    public List<Brand> findAllForBrowser(Brand browser, Date createdDateFrom, Date createdDateTo, Date lastModifiedDateFrom, Date lastModifiedDateTo,
+            long createdUserID, long lastModifiedUserID) {
         List<Brand> list = new ArrayList<>();
 
         try {
-            Query query = helper.getFindAllQuery();
+            Query query = helper.getFindForBrowserQuery(browser.getName(), browser.getDescription(), createdDateFrom, createdDateTo, lastModifiedDateFrom,
+                    lastModifiedDateTo, createdUserID, lastModifiedUserID);
             for (Object object : query.getResultList()) {
                 list.add((Brand) object);
             }
@@ -33,20 +37,20 @@ public class BrandSpecificController {
 
         return list;
     }
-    
+
     public Brand findByName(String name) {
         Brand brand;
-        
+
         try {
             Query query = helper.getFindByNameQuery(name);
             brand = (Brand) query.getSingleResult();
         } catch (Exception e) {
             brand = new BrandNull();
         }
-        
+
         return brand;
     }
-    
+
     public List<Brand> fillSelectorByName(String name) {
         List<Brand> list = new ArrayList<>();
 
