bcf9ce470637e1052fe14734edc946b308e5f0a4
foxtrot-developer <foxtrot-developer@users.noreply.github.com>
2015-07-09 18:40:02 +0200

    Update BaseTypeHelper.java

--- src/java/com/development/simplestockmanager/business/object/helper/BaseTypeHelper.java
+++ src/java/com/development/simplestockmanager/business/object/helper/BaseTypeHelper.java
@@ -3,12 +3,11 @@ package com.development.simplestockmanager.business.object.helper;
 import javax.persistence.Query;
 
 /**
+ * Interface helper class for type object
  *
  * @author foxtrot
  */
 public interface BaseTypeHelper {
-    
-//    public Query getFindByIdQuery(long id);
-    
+
     public Query getFindAllForSelector(String language);
 }
