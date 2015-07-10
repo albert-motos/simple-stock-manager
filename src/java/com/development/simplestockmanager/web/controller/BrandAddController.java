f69a42cc79aeec437d41cdc8b664bd2eb609097f
foxtrot-developer <foxtrot-developer@users.noreply.github.com>
2015-07-10 14:50:56 +0200

    Create BrandAddController.java

---
+++ src/java/com/development/simplestockmanager/web/controller/BrandAddController.java
@@ -0,0 +1,17 @@
+package com.development.simplestockmanager.web.controller;
+
+import com.development.simplestockmanager.web.controller.common.AddController;
+import com.development.simplestockmanager.web.controller.common.BrandCommonController;
+
+/**
+ *
+ * @author albert.motos
+ */
+public class BrandAddController extends BrandCommonController implements AddController {
+
+    @Override
+    public void add() {
+
+    }
+
+}
