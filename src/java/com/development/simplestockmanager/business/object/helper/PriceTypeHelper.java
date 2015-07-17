3cf74d20b96f85168f60f6f014f76062dc841e10
foxtrot-developer <foxtrot-developer@users.noreply.github.com>
2015-07-17 16:47:03 +0200

    Update PriceTypeHelper.java

--- src/java/com/development/simplestockmanager/business/object/helper/PriceTypeHelper.java
+++ src/java/com/development/simplestockmanager/business/object/helper/PriceTypeHelper.java
@@ -1,15 +1,34 @@
 package com.development.simplestockmanager.business.object.helper;
 
 import com.development.simplestockmanager.business.persistence.controller.PriceTypeJpaController;
+import javax.persistence.Query;
 
 /**
  * Helper class for PriceType object
  *
  * @author foxtrot
  */
-public class PriceTypeHelper extends CommonHelper {
+public class PriceTypeHelper extends CommonHelper implements BaseTypeHelper {
 
     public PriceTypeJpaController getJpaController() {
         return new PriceTypeJpaController(entityManagerFactory);
+    }
+
+    @Override
+    public Query getFindByType(String type) {
+        Query query = entityManager.createNamedQuery("PriceType.findByType");
+        query.setParameter("type", type);
+
+        return query;
+    }
+
+    @Override
+    public Query getFindAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Query getFindEnable() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
