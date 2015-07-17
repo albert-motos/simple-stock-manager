a43f37b52c5bb9cf7ed19db3822465a132069680
foxtrot-developer <foxtrot-developer@users.noreply.github.com>
2015-07-17 16:57:08 +0200

    Create PriceTypeCommonController.java

---
+++ src/java/com/development/simplestockmanager/web/controller/common/PriceTypeCommonController.java
@@ -0,0 +1,31 @@
+package com.development.simplestockmanager.web.controller.common;
+
+import com.development.simplestockmanager.business.object.controller.general.PriceTypeGeneralController;
+import com.development.simplestockmanager.business.object.controller.specific.PriceTypeSpecificController;
+import com.development.simplestockmanager.business.persistence.PriceType;
+import com.development.simplestockmanager.web.object.validator.PriceTypeValidator;
+
+/**
+ * Common controller class for PriceType object
+ *
+ * @author foxtrot
+ */
+public class PriceTypeCommonController extends BaseCommonController {
+
+    protected final PriceTypeValidator validator;
+    protected final PriceTypeGeneralController generalController;
+    protected final PriceTypeSpecificController specificController;
+
+    protected PriceType priceType;
+
+    public PriceTypeCommonController(long mode) {
+        generalController = new PriceTypeGeneralController();
+        specificController = new PriceTypeSpecificController();
+        validator = new PriceTypeValidator(mode, languageController, specificController);
+    }
+
+    public PriceType getPriceType() {
+        return priceType;
+    }
+
+}
