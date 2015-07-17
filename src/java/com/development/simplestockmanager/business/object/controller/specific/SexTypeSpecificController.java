2d51bfe2c1efaf696ed83d15de67fa22cbf96c91
foxtrot-developer <foxtrot-developer@users.noreply.github.com>
2015-07-17 16:42:02 +0200

    Update SexTypeSpecificController.java

--- src/java/com/development/simplestockmanager/business/object/controller/specific/SexTypeSpecificController.java
+++ src/java/com/development/simplestockmanager/business/object/controller/specific/SexTypeSpecificController.java
@@ -11,27 +11,12 @@ import javax.persistence.Query;
  *
  * @author foxtrot
  */
-public class SexTypeTypeSpecificController {
+public class SexTypeSpecificController {
 
     private final SexTypeHelper helper;
 
-    public SexTypeTypeSpecificController() {
+    public SexTypeSpecificController() {
         this.helper = new SexTypeHelper();
-    }
-    
-    public List<SexType> findByType(String type) {
-        List<SexType> list = new ArrayList<>();
-
-        try {
-            Query query = helper.getFindByType(type);
-            for (Object object : query.getResultList()) {
-                list.add((SexType) object);
-            }
-        } catch (Exception e) {
-            list = new ArrayList<>();
-        }
-
-        return list;
     }
 
     public List<SexType> findAll() {
@@ -63,4 +48,5 @@ public class SexTypeTypeSpecificController {
 
         return list;
     }
+
 }
